1adf6382925550db09faafb0b5642367f568fd5a
Maxim Grabarnik <maxim@lightricks.com>
2020-11-10 13:40:57 +0200

    Equal: use Zablocki's diff function for compact mismatch message.
    When there is a mismatch of large data-structure objects dumping actual
    and expected values result in a huge amount of hard to read text.
    Diff function outputs only the mismatching fields.

--- Sources/Nimble/Matchers/Equal.swift
+++ Sources/Nimble/Matchers/Equal.swift
@@ -11,7 +11,8 @@ internal func equal<T>(
             return PredicateResult(status: .fail, message: msg)
         case (let expected?, let actual?):
             let matches = areEquivalent(expected, actual)
-            return PredicateResult(bool: matches, message: msg)
+            return matches ? PredicateResult(bool: matches, message: msg) :
+                PredicateResult(bool: matches, message: .fail(diff(expected, actual).joined(separator: "\n")))
         }
     }
 }
@@ -22,6 +23,22 @@ internal func equal<T>(
 /// @see beCloseTo if you want to match imprecise types (eg - floats, doubles).
 public func equal<T: Equatable>(_ expectedValue: T?) -> Predicate<T> {
     equal(expectedValue, by: ==)
+}
+
+/// A Nimble matcher allowing comparison of collection with optional type
+public func equal<T: Equatable>(_ expectedValue: [T?]) -> Predicate<[T?]> {
+    return Predicate.define("equal <\(stringify(expectedValue))>") { actualExpression, msg in
+        guard let actualValue = try actualExpression.evaluate() else {
+            return PredicateResult(
+                status: .fail,
+                message: msg.appendedBeNilHint()
+            )
+        }
+
+        let matches = expectedValue == actualValue
+        return matches ? PredicateResult(bool: matches, message: msg) :
+            PredicateResult(bool: matches, message: .fail(diff(expectedValue, actualValue).joined(separator: "\n")))
+    }
 }
 
 /// A Nimble matcher that succeeds when the actual set is equal to the expected set.
@@ -38,47 +55,21 @@ public func equal<T: Comparable>(_ expectedValue: Set<T>?) -> Predicate<Set<T>> 
 
 private func equal<T>(_ expectedValue: Set<T>?, stringify: @escaping (Set<T>?) -> String) -> Predicate<Set<T>> {
     return Predicate { actualExpression in
-        var errorMessage: ExpectationMessage =
-            .expectedActualValueTo("equal <\(stringify(expectedValue))>")
-
-        guard let expectedValue = expectedValue else {
-            return PredicateResult(
-                status: .fail,
-                message: errorMessage.appendedBeNilHint()
-            )
-        }
-
-        guard let actualValue = try actualExpression.evaluate() else {
-            return PredicateResult(
-                status: .fail,
-                message: errorMessage.appendedBeNilHint()
-            )
-        }
-
-        errorMessage = .expectedCustomValueTo(
-            "equal <\(stringify(expectedValue))>",
-            actual: "<\(stringify(actualValue))>"
-        )
+        let actualValue = try actualExpression.evaluate()
 
         if expectedValue == actualValue {
             return PredicateResult(
                 status: .matches,
-                message: errorMessage
+                message: .expectedCustomValueTo(
+                    "equal <\(stringify(expectedValue))>",
+                    actual: "<\(stringify(actualValue))>"
+                )
             )
         }
 
-        let missing = expectedValue.subtracting(actualValue)
-        if missing.count > 0 {
-            errorMessage = errorMessage.appended(message: ", missing <\(stringify(missing))>")
-        }
-
-        let extra = actualValue.subtracting(expectedValue)
-        if extra.count > 0 {
-            errorMessage = errorMessage.appended(message: ", extra <\(stringify(extra))>")
-        }
         return  PredicateResult(
             status: .doesNotMatch,
-            message: errorMessage
+            message: .fail(diff(expectedValue, actualValue).joined(separator: "\n"))
         )
     }
 }
