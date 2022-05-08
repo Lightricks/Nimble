bdaf716208128128c34ff398ce6cf6acbab5aefa
Netai Benaim <netai@lightricks.com>
2022-05-08 12:03:29 +0300

    Nimble: Use direct module import for Foundations.
    Currently when the module in debug `coped imports are
    not yet supported in module interfaces` warnings are
    produced, these are because Nimble contains enum and
    type specific imports from Foundations. This commit
    imports foundations as is instead of type specific import
    whenever such import was used.

--- Sources/Nimble/DSL.swift
+++ Sources/Nimble/DSL.swift
@@ -114,8 +114,7 @@ internal func internalError(_ msg: String, file: FileString = #file, line: UInt 
 }
 
 #if canImport(Darwin)
-import class Foundation.NSException
-import struct Foundation.NSExceptionName
+import Foundation
 
 private func _nimblePrecondition(
     _ name: String,
